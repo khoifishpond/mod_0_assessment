0659e437c650cd4f39a565a8d14871bc5494ba04
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-17 20:24:18 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,14 @@
+animal = "cat"
+
+is_cuddly = true
+
+num_8 = 8
+num_3 = 3
+
+quotient = num_8 / num_3
+
+favorite_films = ["The Dark Knight", "Interstellar", "Your Name"]
+
+favorite_films.shift
+
+animal.size
